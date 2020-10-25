010fbaa7c2d115c38fb9df833edf76f9c62fe4d4
chicony2753 <chicony2753@gmail.com>
2020-10-25 18:26:09 +0300

    Style header

--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
 from .models import Task, Human
 from .forms import TaskForm, HumanForm, CreateUserForm
 
@@ -20,21 +19,21 @@ def tasks(request):
 
 
 def signIn(request):
-    return render(request, 'main/signIn.html')
+    return render(request, 'main/registration/signIn.html')
 
 
 class signUp(View):
     def get(self, request):
         form = CreateUserForm()
-        return render(request, 'main/signUp.html', {'form': form})
+        return render(request, 'main/registration/signUp.html', {'form': form})
 
     def post(self, request):
-        bound_form = UserCreationForm(request.POST)
+        bound_form = CreateUserForm(request.POST)
 
         if bound_form.is_valid():
             new_user = bound_form.save()
-            return render(request, 'main/signIn.html')
-        return render(request, 'main/signUp.html', {'form': bound_form})
+            return render(request, 'main/registration/signIn.html')
+        return render(request, 'main/registration/signUp.html', {'form': bound_form})
 
 
 def createtask(request):
